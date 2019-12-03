510a40d1a7f2d14fc854060b74d8eeb2e50fe0c5
willbowden37 <william.bowden@aggiemail.usu.edu>
2019-12-02 20:28:17 -0700

    Removed unnecessary stuff in tooltip

--- js/tooltip.js
+++ js/tooltip.js
@@ -9,7 +9,6 @@ class Tooltip {
             .style("position", "absolute")
             .style("z-index", "10")
             .style("visibility", "hidden")
-            // .style("background", "#FFFFFF")
             .attr('id', 'tooltip')
             .classed('tooltipDiv', true)
         ;
@@ -19,22 +18,6 @@ class Tooltip {
      * Gets the HTML content for a tool tip.
      */
     tooltip_html(d) {
-        // let text = "<h2 class ="  + this.chooseClass(d.State_Winner) +
-        //     " >" + d.State + "</h2>";
-        // text +=  "Electoral Votes: " + d.Total_EV;
-        // text += "<ul>"
-        // // Democrat
-        // text += "<li class = democrat>" +
-        //     d.D_Nominee_prop+":\t\t"+d.D_Votes+"("+d.D_Percentage+"%)" + "</li>"
-        // // Republican
-        // text += "<li class = republican>" +
-        //     d.R_Nominee_prop+":\t\t"+d.R_Votes+"("+d.R_Percentage+"%)" + "</li>"
-        // // Independent
-        // if (d.I_Percentage) {
-        //     text += "<li class = independent>" +
-        //         d.I_Nominee_prop+":\t\t"+d.I_Votes+"("+d.I_Percentage+"%)" + "</li>"
-        // }
-        // text += "</ul>";
 
         let text = '<h2>' + d.original_title + '</h2>';
         text += '<ul>';
@@ -47,13 +30,12 @@ class Tooltip {
     mouseover(d) {
         this.tooltip
             .html(this.tooltip_html(d))
-            // .classed('tooltip-title', true)
         ;
         this.tooltip.style("visibility", "visible");
     }
 
     mousemove() {
-        this.tooltip.style("top", (d3.event.pageY-30)+"px")
+        this.tooltip.style("top", (d3.event.pageY-50)+"px")
             .style("left",(d3.event.pageX+10)+"px");
     }
 

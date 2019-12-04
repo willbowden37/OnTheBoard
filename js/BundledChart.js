6b3248decd3e2e38e6aaf25e73e5c38efda1cbba
Joseph <joe.turcotte96@gmail.com>
2019-12-04 13:55:14 -0700

    Finished Project

--- js/BundledChart.js
+++ js/BundledChart.js
@@ -11,7 +11,7 @@ class BundledChart {
             .attr("width", this.svgWidth)
             .attr("height", this.svgHeight)
             .attr("id", "mainSvg")
-        ;
+            ;
 
         this.booklist = [];
         this.clonedNodes = [];
@@ -21,7 +21,7 @@ class BundledChart {
 
 
     }
-    parseData(taggedBooks,genres, ratings){
+    parseData(taggedBooks, genres, ratings) {
         console.log(taggedBooks);
         console.log(genres);
         console.log(ratings);
@@ -56,30 +56,30 @@ class BundledChart {
             //         myConnections = myConnections.concat(booksToConnect)
             //     }
             // })
-            
+
             let myParentIds = []
             genres.forEach(currentGenre => {
-                if(book.tags.includes(currentGenre.id)){
+                if (book.tags.includes(currentGenre.id)) {
                     myParentIds.push(currentGenre.name)
                 }
             })
 
 
             //Assign unique ID for each book
-        
+
             let i = 10
             myParentIds.forEach(parentId => {
-                let uniqueBookId = book.book_id.concat('-'+i.toString(36))
+                let uniqueBookId = book.book_id.concat('-' + i.toString(36))
                 myData.push({
                     id: uniqueBookId,
-                    parentId:parentId,
-                    data:book,
-                    connections:myConnections
+                    parentId: parentId,
+                    data: book,
+                    connections: myConnections
                 })
                 i++
             })
         })
- 
+
         console.log(myData)
         return myData
     }
@@ -88,31 +88,34 @@ class BundledChart {
         // console.log(nodes)
         var map = {},
             links = [];
-        let maxIndividualLinks = 2;
-    
+        var linked = {};
+        let maxIndividualLinks = 4;
+
         // Compute a map from book to book.
-        nodes.forEach(function(d) {
+        nodes.forEach(function (d) {
             map[d.id] = d;
         });
-    
+
         console.log('started')
         // For each import, construct a link from the source to target node.
         nodes.forEach(d => {
+            linked[d.id] = true;
             // console.log(d)
             if (d.data.connections) {
                 for (let i = 0; i < d.data.connections.length && i < maxIndividualLinks; i++) {
                     let conn = d.data.connections[i];
-                    links.push(map[d.id].path(map[conn]));
+                    if (!(conn in linked))
+                        links.push(map[d.id].path(map[conn]));
                 }
             }
         });
         console.log('done')
         // console.log(map)
-        // console.log(links)
+        console.log("Length of links is " + links.length)
         return links;
     }
 
-    initialize(ratings){
+    initialize(ratings) {
 
         let diameter = this.svgHeight * 1.3,
             radius = diameter / 2,
@@ -123,15 +126,15 @@ class BundledChart {
 
         let line = d3.radialLine()
             .curve(d3.curveBundle.beta(0.85))
-            .radius(function(d) { return d.y; })
-            .angle(function(d) { return d.x / 180 * Math.PI; });
+            .radius(function (d) { return d.y; })
+            .angle(function (d) { return d.x / 180 * Math.PI; });
 
         let group = this.svg.append('g')
             .attr("transform", "translate(" + this.svgWidth / 2 + "," + this.svgHeight / 2 + ")")
-        ;
+            ;
 
         this.link = group.append("g").attr('id', 'myMainG').selectAll(".link");
-        let node = group.append("g").selectAll(".node");
+        this.allNode = group.append("g").selectAll(".node");
         let text = group.append("g").selectAll(".parentText");
 
         // console.log('hi')
@@ -141,7 +144,7 @@ class BundledChart {
             .id(d => d.id)
             .parentId(d => d.parentId)
             (ratings)
-        ;
+            ;
         // console.log(root)
         cluster(root)
 
@@ -156,37 +159,39 @@ class BundledChart {
         this.link = this.link
             .data(myLinks)
             .enter().append("path")
-            .each(function(d) { 
+            .each(function (d) {
                 //console.log(d)
-                d.source = d[0], d.target = d[d.length - 1]; 
+                d.source = d[0], d.target = d[d.length - 1];
             })
             .attr("class", "link")
             .attr("d", line)
             .attr("id", d => d[0].id + "-" + d[d.length - 1].id)
-        ;
+            ;
         console.log("Done with links");
 
         let barWidth = 10
         let barHeight = .1
-        node = node
+        this.allNode = this.allNode
             .data(root.leaves())
             .enter().append("rect")
             .attr("class", "node")
             .attr('width', barWidth)
             .attr('height', barHeight)
             .attr('class', d => d.parentId)
-            .attr("transform", function(d) { return "rotate(" + (d.x - 90) + ")translate(" + (d.y + 8) + ",0)"; })
-            // .attr("text-anchor", function(d) { return d.x < 180 ? "start" : "end"; })
+            .attr("transform", function (d) { return "rotate(" + (d.x - 90) + ")translate(" + (d.y + 8) + ",0)"; })
+            .attr("id", d => d.data.data.book_id)
+            // 
             // .text(function(d) { return d.id; })
-        ;
+            ;
 
         text = text.data(myGenres)
             .enter()
             .append('text')
             .attr('class', 'barChartTitle')
-            .attr('dx',d => d.y * 2.1 * Math.cos((d.x - 90) * Math.PI / 180))
+            .attr('dx', d => d.y * 2.1 * Math.cos((d.x - 90) * Math.PI / 180))
             .attr('dy', d => d.y * 2.1 * Math.sin((d.x - 90) * Math.PI / 180))
             .text(d => d.id)
+            .attr("text-anchor", function(d) { return d.x < 180 ? "start" : "end"; })
 
     }
 
@@ -196,24 +201,24 @@ class BundledChart {
             let rn = document.getElementById(node);
             if (rn) rn.remove();
         });
-        if(added) {
+        if (added) {
             this.booklist.push(book);
         } else {
             let deleteIndex = -1;
-            for(let i = 0; i < this.booklist.length; i++) {
-                if(this.booklist[i].original_title === book.original_title) {
+            for (let i = 0; i < this.booklist.length; i++) {
+                if (this.booklist[i].original_title === book.original_title) {
                     deleteIndex = i;
                 }
             }
-            if(deleteIndex>=0) this.booklist.splice(deleteIndex, 1);
+            if (deleteIndex >= 0) this.booklist.splice(deleteIndex, 1);
         }
 
-        for(let eachLink of this.link._groups[0]) {
+        for (let eachLink of this.link._groups[0]) {
             let bookIds = eachLink.id.split('-');
             let book1Id = bookIds[0];
-            let book2Id = bookIds[1];
-            for(let eachBook of this.booklist) {
-                if(eachBook.book_id === book1Id || eachBook.book_id === book2Id) {
+            let book2Id = bookIds[2];
+            for (let eachBook of this.booklist) {
+                if (eachBook.book_id === book1Id || eachBook.book_id === book2Id) {
                     eachLink.classList.add('selected');
                     let node = document.getElementById(eachLink.id);
                     let newNode = node.cloneNode(true);
@@ -223,9 +228,9 @@ class BundledChart {
                     document.querySelector("#myMainG").appendChild(newNode);
                 }
             }
-            if(!added) {
-                if(book.book_id === book1Id || book.book_id === book2Id) {
-                    if(eachLink.classList.contains('selected')) {
+            if (!added) {
+                if (book.book_id === book1Id || book.book_id === book2Id) {
+                    if (eachLink.classList.contains('selected')) {
                         eachLink.classList.remove('selected');
                     }
                 }
@@ -234,37 +239,22 @@ class BundledChart {
     }
 
     updateHover(book, added) {
-        this.clonedNodesHover.forEach(node => {
-            let rn = document.getElementById(node);
-            if (rn) rn.remove();
-        });
-        if(added) {
-            this.booklistHover.push(book);
-        } else {
-            let deleteIndex = -1;
-            for(let i = 0; i < this.booklistHover.length; i++) {
-                if(this.booklistHover[i].original_title === book.original_title) {
-                    deleteIndex = i;
+        //for (let i = 0; i < this.node)
+        if (added) {
+            console.log(this.allNode);
+            for (let node of this.allNode._groups[0]) {
+                if (book.book_id === node.id) {
+                    node.classList.add("selectedHover");
                 }
             }
-            if(deleteIndex>=0) this.booklistHover.splice(deleteIndex, 1);
+        } else {
+            for (let node of this.allNode._groups[0]) {
+                if (book.book_id === node.id) {
+                    node.classList.remove("selectedHover");
+                }
+            }
         }
 
-        for(let eachLink of this.link._groups[0]) {
-            let bookIds = eachLink.id.split('-');
-            let book1Id = bookIds[0];
-            let book2Id = bookIds[1];
-            for(let eachBook of this.booklistHover) {
-                if(eachBook.book_id === book1Id || eachBook.book_id === book2Id) {
-                    let node = document.getElementById(eachLink.id);
-                    let newNode = node.cloneNode(true);
-                    let newId = eachLink.id + "-clone-hover";
-                    this.clonedNodesHover.push(newId);
-                    newNode.id = newId;
-                    newNode.classList.add("selectedHover");
-                    document.querySelector("#myMainG").appendChild(newNode);
-                }
-            }
-        }
+
     }
 }
